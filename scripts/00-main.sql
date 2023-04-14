1119c3d49be727dd1a40dc2a867c6b188ecbebf3
Moritz Utcke <62291876+Letsmoe@users.noreply.github.com>
2023-04-14 23:32:37 +0400

    Users

--- scripts/00-main.sql
+++ scripts/00-main.sql
@@ -6,6 +6,7 @@ CREATE TABLE links(
 	shortcode varchar(8) NOT NULL,
 	"views" integer NOT NULL DEFAULT 0,
 	hosts_id integer NOT NULL,
+	users_id integer NOT NULL,
 	CONSTRAINT links_pkey PRIMARY KEY(id)
 );
 
@@ -15,10 +16,20 @@ CREATE TABLE hosts(
 	CONSTRAINT hosts_pkey PRIMARY KEY(id)
 );
 
-CREATE TABLE views(
+CREATE TABLE "views"(
 	date timestamp NOT NULL DEFAULT NOW(),
 	links_id integer NOT NULL,
+	users_id integer,
 	CONSTRAINT views_pkey PRIMARY KEY(links_id, date)
+);
+
+CREATE TABLE users(
+	id serial NOT NULL,
+	username varchar(60) NOT NULL,
+	email varchar(255) NOT NULL,
+	"password" varchar NOT NULL,
+	uid uuid NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY(id)
 );
 
 ALTER TABLE
@@ -30,5 +41,15 @@ ALTER TABLE
 	"views"
 ADD
 	CONSTRAINT views_links_id_fkey FOREIGN KEY (links_id) REFERENCES links (id);
+
+ALTER TABLE
+	links
+ADD
+	CONSTRAINT links_users_id_fkey FOREIGN KEY (users_id) REFERENCES users (id);
+
+ALTER TABLE
+	"views"
+ADD
+	CONSTRAINT views_users_id_fkey FOREIGN KEY (users_id) REFERENCES users (id);
 
 END;
